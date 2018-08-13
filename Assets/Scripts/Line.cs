d962d8b74cc505628a9afdbe1e4f43c58fae529a
Clay Ewing <c.ewing@miami.edu>
2018-08-13 15:40:41 -0400

    updating line renderer

--- Assets/Scripts/Line.cs
+++ Assets/Scripts/Line.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -7,25 +7,35 @@ public class Line : MonoBehaviour {
 	public List<Vector3> pointsList;
 	public GameObject point;
 
+	//The First and Second Color for the line gradient
+	public Color color1 = new Color(0.97f, 0.23f, 0.98f, 1.00f);
+	public Color color2 = new Color(0.21f, 0.86f, 0.86f, 1.00f);
+	public float fadeInTime = 3f;
+	public float fadeOutTime = 3f;
+    public Material material;
+
+
+        
 	struct myLine {
 		public Vector3 StartPoint;
 		public Vector3 EndPoint;
-	};
+	}
 
 	void Awake () {
 		// Create line renderer component and set its property
 		line = gameObject.AddComponent<LineRenderer> ();
-		line.material = new Material (Shader.Find ("Sprites/Default"));
-		Color whiteAlpha = new Color(255f,255f,255f,150f);
-		line.material.color = whiteAlpha;
-		line.SetVertexCount (0);
+        // Give the line a default shader
+        //		line.material = new Material(Shader.Find ("Assets/Sprites/Pieces/Outline.mat"));
+        line.material = material;
+
+        line.SetVertexCount (0);
 		line.SetWidth (0.1f, 0.15f);
 		line.SetColors (Color.white, Color.white);
 		line.useWorldSpace = true;    
 		pointsList = new List<Vector3> ();
 		line.SetVertexCount (0);
 		pointsList.RemoveRange (0, pointsList.Count);
-		line.SetColors (Color.white, Color.white);
+		//line.SetColors (Color.white, Color.white);
 	}
 
 	public bool Shorten() {
@@ -37,9 +47,7 @@ public class Line : MonoBehaviour {
 			//remove point edges
 			EdgeCollider2D[] points =  gameObject.GetComponentsInChildren<EdgeCollider2D>();
 			Destroy(points[0].gameObject);
-//			points[0].enabled = false;
-
-
+//			points[0].enabled = false;         
 		}
 		else {
 			Destroy(this.gameObject);
@@ -64,10 +72,35 @@ public class Line : MonoBehaviour {
 	}
 	// Use this for initialization
 	void Start () {
-	
+		StartCoroutine(ColorChangeIn());
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	IEnumerator ColorChangeIn() {
+		// Fade the Colors into each other
+		for (float t = 0.01f; t < fadeInTime; t += 0.1f)
+		{
+			line.startColor = Color.Lerp(color1, color2, t/fadeInTime); 
+			line.endColor = Color.Lerp(color2, color1, t/fadeInTime);
+			yield return null;
+		}
+		yield return new WaitForSeconds(.5f);
+		StartCoroutine(ColorChangeOut());
 	}
+
+
+	IEnumerator ColorChangeOut() {
+		// Go back to the first color assigned
+		for (float t = 0.01f; t < fadeOutTime; t += 0.1f)
+		{
+			line.startColor = Color.Lerp(color2, color1, t / fadeInTime);
+            line.endColor = Color.Lerp(color1, color2, t / fadeInTime);
+            yield return null;
+		}
+		yield return new WaitForSeconds(.5f);
+		//Repeat
+        StartCoroutine(ColorChangeIn());
+	}
+
+
+
 }
