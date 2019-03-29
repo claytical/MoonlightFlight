4a4cec9d755d095fcdcb9754f71c9528d3e9d19d
Clay Ewing <c.ewing@miami.edu>
2019-03-29 16:10:01 -0400

    last update

--- Assets/Drift.cs
+++ Assets/Drift.cs
@@ -7,12 +7,14 @@ public class Drift : MonoBehaviour
     public bool randomDrift = false;
     private float angle = 0f;
     private float speed = 5f;
+    private float mass;
     private Vector3 startPosition;
     private Vector3 destination;
     private float amount;
     // Use this for initialization
     void Start()
     {
+        mass = Random.Range(.01f, .05f);
         startPosition = transform.position;
         if (randomDrift)
         {
@@ -40,16 +42,20 @@ public class Drift : MonoBehaviour
         {
             Vector3 topLeftBoundary = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, Camera.main.farClipPlane / 2));
             Vector3 bottomRightBoundary = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.farClipPlane / 2));
-            if(transform.position.x > topLeftBoundary.x && transform.position.x < bottomRightBoundary.x)
-            {
-                Vector3 xPos = new Vector3(-Input.acceleration.x, 0, 0);
+          //  if(transform.position.x >= topLeftBoundary.x && transform.position.x <= bottomRightBoundary.x)
+          //  {
+                Vector3 xPos = new Vector3(Input.acceleration.x * mass, 0, 0);
                 transform.position = transform.position + xPos;
-            }
-            if (transform.position.y > topLeftBoundary.y && transform.position.y < bottomRightBoundary.y)
-            {
-                Vector3 yPos = new Vector3(0, -Input.acceleration.y, 0);
+          //  }
+         //   if (transform.position.y >= topLeftBoundary.y && transform.position.y <= bottomRightBoundary.y)
+         //   {
+                Vector3 yPos = new Vector3(0, Input.acceleration.y * mass, 0);
                 transform.position = transform.position + yPos;
-            }
+         //   }
+         //   else
+//            {
+
+  //          }
         }
     }
 }
